afcbfe44e91272618138b58c3319bfeefe95d229
Nick <nearwood@gmail.com>
2019-04-02 15:36:05 -0400

    ok that was easy

--- zeros.go
+++ zeros.go
@@ -3,6 +3,8 @@ package main
 import (
   "log"
   "os"
+  "io"
+  "fmt"
 )
 
 func main() {
@@ -10,6 +12,35 @@ func main() {
   if err != nil {
     log.Fatal(err)
   }
+
+  count := 0
+  total := 0
+  data := make([]byte, 4096)
+
+  for {
+    n, err := f.Read(data);
+
+    if n > 0 {
+      total += n
+      for _, b := range data {
+        if b == 0x0 {
+          count += 1
+        }
+      }
+    }
+
+    if err == io.EOF {
+      break;
+    }
+
+    if err != nil {
+      log.Fatal(err)
+    }
+  }
+
+  fmt.Printf("Zero byte count: %d\n", count)
+  fmt.Printf("Total file bytes: %d\n", total)
+  fmt.Printf("%3.3f%% empty\n", float64(count)/float64(total) * 100)
 
   if err := f.Close(); err != nil {
     log.Fatal(err)
